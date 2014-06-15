011462697fa353731f23a82107bd5ba5874fa9e2
Marek Rudnicki <marekrud@gmail.com>
2014-06-15 13:10:11 +0200

    version 0.2

--- matlab_wrapper/__init__.py
+++ matlab_wrapper/__init__.py
@@ -1,3 +1,5 @@
 from __future__ import division, print_function, absolute_import
 
+__version__ = "0.2"
+
 from matlab_wrapper.matlab_session import MatlabSession
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup,find_packages
 
 setup(
     name = "matlab_wrapper",
-    version = "0.1",
+    version = "0.2",
     packages = find_packages(),
 
     author = "Marek Rudnicki",
