636fce9dda90aa0ede4a6702272fd39bdf152d12
Marek Rudnicki <marekrud@gmail.com>
2014-06-15 22:37:34 +0200

    setup.py updates

--- setup.py
+++ setup.py
@@ -2,16 +2,31 @@
 
 from setuptools import setup,find_packages
 
+with open('README.rst') as file:
+    long_description = file.read()
+
 setup(
     name = "matlab_wrapper",
     version = "0.2",
-    packages = find_packages(),
-
     author = "Marek Rudnicki",
     author_email = "marekrud@gmail.com",
+
     description = "MATLAB wrapper",
     license = "GPLv3",
+    url = "https://github.com/mrkrd/matlab_wrapper",
 
-    # platforms=["Linux", "Windows"],
-    # install_requires=["setuptools", "numpy"],
+    packages = find_packages(),
+    long_description = long_description,
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+    ],
+
+    platforms = ["Linux"],
+    install_requires=["numpy"],
 )
