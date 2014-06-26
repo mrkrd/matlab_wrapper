9ae87f8cfd77c3f06117fbdc42ac8c0c58dc8256
Marek Rudnicki <marekrud@gmail.com>
2014-06-27 01:36:51 +0200

    random plot example

---
+++ examples/random_plot.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Let's plot something random in MATLAB.
+
+"""
+
+from __future__ import division, absolute_import, print_function
+
+__author__ = "Marek Rudnicki"
+
+import numpy as np
+
+import matlab_wrapper
+
+def main():
+    matlab = matlab_wrapper.MatlabSession()
+
+    x = np.random.randn(100)
+
+    matlab.workspace.plot(x)
+
+    raw_input("Press enter to finish...")
+
+
+if __name__ == "__main__":
+    main()
