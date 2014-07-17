1cfa4e73ebb0b66efea9b61df918e57961176141
Marek Rudnicki <marekrud@gmail.com>
2014-07-17 17:16:59 +0200

    leak loop tests

---
+++ tests/leak_loops/putget_loop.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Keep putting and getting a variable to MATLAB's workspace.
+
+"""
+
+from __future__ import division, absolute_import, print_function
+
+__author__ = "Marek Rudnicki"
+
+
+import numpy as np
+
+import matlab_wrapper
+import psutil
+
+
+def main():
+    matlab = matlab_wrapper.MatlabSession()
+
+    p = psutil.Process()
+
+    i = 0
+    while True:
+        print(i, p.memory_percent(), p.memory_info())
+
+        a = np.random.randn(1e7)
+
+        matlab.put('a', a)
+        matlab.get('a')
+
+        i += 1
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/leak_loops/restart_loop.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Keep restarting MATLAB.
+
+"""
+
+from __future__ import division, absolute_import, print_function
+
+__author__ = "Marek Rudnicki"
+
+
+import matlab_wrapper
+import psutil
+
+
+def main():
+
+    p = psutil.Process()
+
+    i = 0
+    while True:
+        print(i, p.memory_percent(), p.memory_info())
+
+        matlab = matlab_wrapper.MatlabSession()
+
+        i += 1
+
+
+if __name__ == "__main__":
+    main()
