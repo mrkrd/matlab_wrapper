5761c0c597dfa15e0c409c0f496da247740c0f5c
Marek Rudnicki <marekrud@gmail.com>
2014-07-21 13:54:58 +0200

    freqz example

---
+++ examples/fir_filter.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+"""Design a lowpas FIR filter.  Based on an example from `freqz`
+documentation.
+
+"""
+
+from __future__ import division, print_function, absolute_import
+from __future__ import unicode_literals
+
+__author__ = "Marek Rudnicki"
+__copyright__ = "Copyright 2014, Marek Rudnicki"
+__license__ = "GPLv3+"
+
+
+import numpy as np
+import matlab_wrapper
+
+
+def main():
+
+    matlab = matlab_wrapper.MatlabSession()
+    w = matlab.workspace
+
+    kaiser = w.kaiser(81., 8.)
+
+    b = w.fir1(80., 0.5, kaiser)
+
+    w.freqz(b, 1., nout=0)
+
+    raw_input("Press enter to finish...")
+
+
+if __name__ == "__main__":
+    main()
