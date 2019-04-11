9c42de0c379d7f07d9849b7d4770fba70ec8a6f4
mr <>
2019-04-11 15:32:21 +0200

    v1

--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python
 
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 
 with open('README.rst') as file:
     long_description = file.read()
 
 setup(
-    name = "matlab_wrapper",
-    version = "0.9.8",
-    author = "Marek Rudnicki",
-    author_email = "marekrud@posteo.de",
+    name="matlab_wrapper",
+    version="1",
+    author="Marek Rudnicki",
+    author_email="marekrud@posteo.de",
 
-    description = "MATLAB wrapper for Python",
-    license = "GPLv3",
-    url = "https://github.com/mrkrd/matlab_wrapper",
-    download_url = "https://github.com/mrkrd/matlab_wrapper/tarball/master",
+    description="MATLAB wrapper for Python",
+    license="GPLv3",
+    url="https://github.com/mrkrd/matlab_wrapper",
+    download_url="https://github.com/mrkrd/matlab_wrapper/tarball/master",
 
-    packages = find_packages(),
-    long_description = long_description,
-    classifiers = [
+    packages=find_packages(),
+    long_description=long_description,
+    classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Console",
         "Intended Audience :: End Users/Desktop",
@@ -33,6 +33,6 @@ setup(
         "Programming Language :: Python :: 2.7",
     ],
 
-    platforms = ["Linux", "Windows", "OSX"],
+    platforms=["Linux", "Windows", "OSX"],
     install_requires=["numpy"],
 )
